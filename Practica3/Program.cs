c1a3938fb50d1516d482cf54c01b3a6584df5f63
jonathan Hiriart <hiriartjonathan@gmail.com>
2025-03-26 18:40:13 -0300

    agregando practica 3

---
+++ Practica3/Program.cs
@@ -0,0 +1,8 @@
+﻿// inicio de clase 3 
+// los var son de tipo anonimo es decir que no tienen tipos 
+var x = new {nombre= "juan", edad = 20};
+Console.WriteLine(x.GetType());
+dynamic d = "hola";
+Console.WriteLine(d.GetType());
+d = 12;
+Console.WriteLine(d.GetType());
